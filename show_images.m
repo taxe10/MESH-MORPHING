0483412260560bd1b73fd6436978c7258c7e1517
taxe10 <56451661+taxe10@users.noreply.github.com>
2020-06-10 14:58:24 -0500

    Update show_images.m

--- show_images.m
+++ show_images.m
@@ -1,8 +1,11 @@
 function show_images(img_path,img_THz)
+SS = get(0,'screensize');
+SS(3) = round(SS(3)*0.48);
+SS(4) = round(SS(4)*0.89);
 figure(2)
 imshow(img_path);
-set(gcf,'position',[775 0 775 1000]);
+set(gcf,'position',[SS(4) 45 SS(4) SS(3)]);
 figure(1)
 imshow(img_THz);
-set(gcf,'position',[0 0 775 1000]);
+set(gcf,'position',[0 45 SS(4) SS(3)]);
 end
